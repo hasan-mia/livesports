cfa08f9f3df727c4dd924ceea18433f0008598d8
hasanrafi69 <hasanrafi69@gmail.com>
2022-03-04 08:51:14 +0600

    fetch data

--- js/index.js
+++ js/index.js
@@ -5,7 +5,11 @@
 * Mobile: +880-161-782323
 */
 
-const liveSports = ()=>{
+//====================
+//Football Score Data
+//====================
+const liveFootball = ()=>{
+
 	fetch(
     "https://livescore6.p.rapidapi.com/matches/v2/list-live?Category=soccer",
     {
@@ -17,14 +21,79 @@ const liveSports = ()=>{
     }
   )
     .then((res) => res.json())
-    .then((data) => loadSporst(data.Stages))
-    .catch((err) => {
-      console.error(err);
-    });
+    .then((data) => loadFootball(data.Stages))
+    .catch((err) => console.error(err))
 }
-const loadSporst =(livescores)=>{
+
+const loadFootball =(livescores)=>{
   livescores.forEach(livescore=>{
-    //All Dasta
-		console.log(livescore);
+    // livescore.Events.forEach(match=>{
+    //   console.log(match)
+    //   console.log(match.Stg.Ccd)
+    //   console.log(match.Stg.Ccdiso)
+    //   console.log(match.Stg.Cid)
+    //   console.log(match.Stg.Sdn)
+    // })
+
+    console.log(livescore.Events)
+
+
+	});
+}
+
+//====================
+//Cricket Score Data
+//====================
+const liveCricket = ()=>{
+
+// Cricbuzz Cricket Score
+//   fetch("https://unofficial-cricbuzz.p.rapidapi.com/matches/list?matchState=recent", {
+// 	"method": "GET",
+// 	"headers": {
+// 		"x-rapidapi-host": "unofficial-cricbuzz.p.rapidapi.com",
+// 		"x-rapidapi-key": "4427861b7bmshec95f48abab1638p147b9bjsn1ee2aa892194"
+// 	}
+// })
+// .then(response => response.json())
+// .then(data => loadSporst(data.typeMatches ?? 'error'))
+// .catch(err =>console.error(error));
+
+}
+const loadCricket =(livescores)=>{
+  livescores.forEach(livescore=>{
+   
+    //All Data of cricbuzz
+		// console.log(livescore);
+		// console.log(livescore.matchType);
+		// console.log(livescore.seriesAdWrapper);
+		// for (const iterator of livescore.seriesAdWrapper) {
+    //   console.log(iterator.seriesMatches.matches)
+    //   console.log(iterator.seriesMatches.matches.forEach(m=>{
+    //     console.log(m.matchInfo.seriesName)
+    //     console.log(m.matchInfo.matchDesc)
+    //     console.log(m.matchInfo.matchFormat)
+    //     console.log(m.matchInfo.state)
+    //     console.log(m.matchInfo.status)
+    //     console.log(m.matchInfo.venueInfo.ground)
+    //     console.log(m.matchInfo.venueInfo.timezone)
+    //     // console.log(m.matchInfo.team1)
+    //     console.log(m.matchInfo.team1.teamName)
+    //     // console.log(m.matchInfo.team2)
+    //     console.log(m.matchInfo.team2.teamName)
+    //     console.log(m.matchScore)
+    //     console.log(m.matchScore.team1Score)
+    //     console.log(m.matchScore.team1Score.inngs1)
+    //     console.log(m.matchScore.team1Score.inngs1.overs)
+    //     console.log(m.matchScore.team1Score.inngs1.runs)
+    //     console.log(m.matchScore.team1Score.inngs1.wickets)
+    //     console.log(m.matchScore.team2Score)
+    //     console.log(m.matchScore.team2Score.inngs1)
+    //     console.log(m.matchScore.team2Score.inngs1.overs)
+    //     console.log(m.matchScore.team2Score.inngs1.runs)
+    //     console.log(m.matchScore.team2Score.inngs1.wickets)
+    //   }))
+    //   // console.log(iterator.seriesMatches)
+    // }
+
 	});
 }
---
+++ test.js
@@ -0,0 +1,6 @@
+const kvArray = [{ key: 1, value: 10 },
+                 { key: 2, value: 20 },
+                 { key: 3, value: 30 }];
+
+const reformattedArray = kvArray.map([value]);
+console.log(reformattedArray)
